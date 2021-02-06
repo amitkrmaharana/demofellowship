8a14f1bfed3fe6b197046b7ef3d6d625b13bbbbf
amitkrmaharana <ak657harley@gmail.com>
2021-02-06 10:24:58 +0530

    Added: New SOP is added

--- HelloJava.java
+++ HelloJava.java
@@ -2,6 +2,11 @@ package javapackage;
 
 public class HelloJava {
 	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		int c;
+		c=a+b;
+		System.out.println("Addition is "+c);
 		System.out.println("Hello Java");
 	}
 }
